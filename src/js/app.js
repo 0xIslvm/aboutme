c9131d6893d836076ac659c9e4f277648f106afd
Islam Essam <89331058+Cyber-Punisher@users.noreply.github.com>
2023-10-20 01:53:57 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,71 +1,118 @@
-{
-  "particles": {
-    "number": {
-      "value": 20,
-      "density": {
-        "enable": true,
-        "value_area": 1000
-      }
-    },
-    "color": {
-      "value": "#f1f3f5"
-    },
-    "shape": {
-      "type": "circle"
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false
-      }
-    },
-    "size": {
-      "value": 0.1,
-      "random": false,
-      "anim": {
-        "enable": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 200,
-      "color": "#f1f3f5",
-      "opacity": 0.5,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 5,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "attract": {
-        "enable": false
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
+/* sweetScroll load */
+document.addEventListener("DOMContentLoaded", function () {
+  new SweetScroll({/* some options */});
+
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS('particles-js', {
+    "particles": {
+      "number": {
+        "value": 30,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
       },
-      "onclick": {
-        "enable": false
+      "color": {
+        "value": "#ffffff"
       },
-      "resize": true
+      "shape": {
+        "type": "polygon",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 19.18081918081918,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 4,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      },
+      nb: 80
     },
-    "modes": {
-      "grab": {
-        "distance": 200,
-        "line_linked": {
-          "opacity": 1
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
         }
       }
-    }
-  },
-  "retina_detect": true
-}
+    },
+    "retina_detect": true
+  });
+
+}, false);
